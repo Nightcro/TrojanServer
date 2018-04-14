b91bd68a095b5353fcb64946cb497abf22e3afee
Mihai <podarumihai97@gmail.com>
2018-04-14 16:03:56 +0300

    Da

---
+++ TroianServer/Messages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TroianServer
+{
+    class Messages
+    {
+        public static String ReceiveMessage(NetworkStream stream)
+        {
+            Byte[] dir = new byte[1024];
+            int size = stream.Read(dir, 0, dir.Length);
+
+            Byte[] aux = new byte[size + 1];
+            Array.Copy(dir, aux, size);
+            String msg = Encoding.ASCII.GetString(aux);
+
+            return msg;
+        }
+
+        public static void SendMessage(NetworkStream stream, String msg)
+        {
+            Byte[] bytes = Encoding.ASCII.GetBytes(msg);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
--- TroianServer/Program.cs
+++ TroianServer/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Diagnostics;
 
 namespace TroianServer
 {
@@ -17,32 +18,18 @@ namespace TroianServer
             int port = 30021;
             TcpListener server = new TcpListener(System.Net.IPAddress.Parse("192.168.21.89"), port);
             TcpClient client;
-
+            String username;
+            
             try
             {
                 server.Start();
-
+                Console.WriteLine("Waiting for a connection...");
+                client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                
                 while (true)
                 {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    Byte[] bytes = new byte[1024];
-                    // Loop to receive all the data sent by the client.
-                    i = stream.Read(bytes, 0, bytes.Length);
-                    String msg = bytes.ToString();
-
-                    Console.WriteLine(msg);
-                    client.Close();
+                    username = Messages.ReceiveMessage(stream);
                 }
             }
             catch (Exception e) {
